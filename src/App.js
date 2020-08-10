3931b80af675fb483fd72e46b24f9e93b77699f6
Andy Sanchez <andysanchez726@gmail.com>
2020-08-09 21:15:43 -0500

    started about me

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
+import About from './components/About Me'
 
 function App() {
   return (
     <div>
-    <Header></Header>
+      <Header></Header>
+      <main>
+        <div>
+          <About></About>
+        </div>
+      </main>
     </div>
+
   );
 }
 
