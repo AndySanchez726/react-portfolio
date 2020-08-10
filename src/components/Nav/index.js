c68fff6e6cc91ee5a40bc81061117b9adeb67e08
Andy Sanchez <andysanchez726@gmail.com>
2020-08-09 21:08:17 -0500

    started nav, pausing to do the other sections

---
+++ src/components/Nav/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <nav>
+            <ul className='flex-row'>
+                <li className='mx-2'>
+                    <a href='#about'>
+                        About Me
+                    </a>
+                </li>
+                <li>
+                
+            </ul>
+        </nav>
+    )
+}
