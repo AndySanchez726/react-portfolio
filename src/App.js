43d85d8959bfc519ae07148e0a4f0f404815496c
Andy Sanchez <andysanchez726@gmail.com>
2020-08-10 21:26:08 -0500

    added contact form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Header from './components/Header';
 import About from './components/About Me'
-import Footer from './components/Footer'
+import Footer from './components/Footer';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main>
         <div>
           <About></About>
+          <Contact></Contact>
           <Footer></Footer>
         </div>
       </main>
