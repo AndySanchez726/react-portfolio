7775e02173cbd01de4b870b38dbefc30d77ec1dc
Andy Sanchez <andysanchez726@gmail.com>
2021-09-14 18:31:29 -0500

    Updates

---
+++ precache-manifest.a9eb02b90397f78c7f7e1d43c824a4f4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f888739a721107d916e5a3d9a72b20",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "8739f6e939053b924a9f",
+    "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ca7f199408ae123270f7",
+    "url": "/react-portfolio/static/css/main.cc5acb49.chunk.css"
+  },
+  {
+    "revision": "8739f6e939053b924a9f",
+    "url": "/react-portfolio/static/js/2.be06b934.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-portfolio/static/js/2.be06b934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca7f199408ae123270f7",
+    "url": "/react-portfolio/static/js/main.682704fc.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "62f49e645bfef9fc882242af645bbfda",
+    "url": "/react-portfolio/static/media/0.62f49e64.png"
+  },
+  {
+    "revision": "e823f1e9c444853f97e657cef657698c",
+    "url": "/react-portfolio/static/media/1.e823f1e9.png"
+  },
+  {
+    "revision": "c00ae73d0291c4b0bbd0a39f646e3fb1",
+    "url": "/react-portfolio/static/media/2.c00ae73d.png"
+  },
+  {
+    "revision": "1699971c7ae26acf02e86212b365af8b",
+    "url": "/react-portfolio/static/media/3.1699971c.png"
+  },
+  {
+    "revision": "a3d8e2313d85ef78af6ee0d304587f5b",
+    "url": "/react-portfolio/static/media/4.a3d8e231.png"
+  },
+  {
+    "revision": "5c736afbb1bd1006b72d9ba47eb54728",
+    "url": "/react-portfolio/static/media/6.5c736afb.png"
+  },
+  {
+    "revision": "4fb4b6901d6c8718c031d568aa660343",
+    "url": "/react-portfolio/static/media/AndySanchez_Resume.4fb4b690.pdf"
+  },
+  {
+    "revision": "f1e7d15a1d984158334418cd97434e61",
+    "url": "/react-portfolio/static/media/andy_2.f1e7d15a.png"
+  },
+  {
+    "revision": "bfd3874ebef96823d69f7243768942a3",
+    "url": "/react-portfolio/static/media/family.bfd3874e.png"
+  }
+]);
