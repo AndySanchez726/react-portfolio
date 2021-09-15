81875393336b9aae80a3c01ffd451f294ceff26f
Andy Sanchez <andysanchez726@gmail.com>
2021-09-14 22:27:29 -0500

    Updates

---
+++ precache-manifest.b5306c1f99418a8920527ca5e2f8dffc.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7265dc40a2e26a40794d76099b0ee9cb",
+    "url": "/andy-sanchez.com/index.html"
+  },
+  {
+    "revision": "8739f6e939053b924a9f",
+    "url": "/andy-sanchez.com/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e711c459b2748428a2de",
+    "url": "/andy-sanchez.com/static/css/main.cc5acb49.chunk.css"
+  },
+  {
+    "revision": "8739f6e939053b924a9f",
+    "url": "/andy-sanchez.com/static/js/2.be06b934.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/andy-sanchez.com/static/js/2.be06b934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e711c459b2748428a2de",
+    "url": "/andy-sanchez.com/static/js/main.9eb819d8.chunk.js"
+  },
+  {
+    "revision": "12d696be1e4efd150806",
+    "url": "/andy-sanchez.com/static/js/runtime-main.98a4ecc6.js"
+  },
+  {
+    "revision": "62f49e645bfef9fc882242af645bbfda",
+    "url": "/andy-sanchez.com/static/media/0.62f49e64.png"
+  },
+  {
+    "revision": "e823f1e9c444853f97e657cef657698c",
+    "url": "/andy-sanchez.com/static/media/1.e823f1e9.png"
+  },
+  {
+    "revision": "c00ae73d0291c4b0bbd0a39f646e3fb1",
+    "url": "/andy-sanchez.com/static/media/2.c00ae73d.png"
+  },
+  {
+    "revision": "1699971c7ae26acf02e86212b365af8b",
+    "url": "/andy-sanchez.com/static/media/3.1699971c.png"
+  },
+  {
+    "revision": "a3d8e2313d85ef78af6ee0d304587f5b",
+    "url": "/andy-sanchez.com/static/media/4.a3d8e231.png"
+  },
+  {
+    "revision": "5c736afbb1bd1006b72d9ba47eb54728",
+    "url": "/andy-sanchez.com/static/media/6.5c736afb.png"
+  },
+  {
+    "revision": "4fb4b6901d6c8718c031d568aa660343",
+    "url": "/andy-sanchez.com/static/media/AndySanchez_Resume.4fb4b690.pdf"
+  },
+  {
+    "revision": "f1e7d15a1d984158334418cd97434e61",
+    "url": "/andy-sanchez.com/static/media/andy_2.f1e7d15a.png"
+  },
+  {
+    "revision": "bfd3874ebef96823d69f7243768942a3",
+    "url": "/andy-sanchez.com/static/media/family.bfd3874e.png"
+  }
+]);
